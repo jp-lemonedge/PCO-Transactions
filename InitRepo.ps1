0d504445c02c18ef3acbb3b102b7793fd89d2444
jp-lemonedge <jean-pierre@lemonedge.com>
2022-04-02 18:07:56 +0100

    2nd Commit

---
+++ InitRepo.ps1
@@ -0,0 +1,12 @@
+# ------------------------------------------------------------------------------
+#                     Author    : LemonEdge - JPD
+#                     Time-stamp: "2022-04-02 16:55:55 jpdur"
+# ------------------------------------------------------------------------------
+
+echo "# PCO-Transactions" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jp-lemonedge/PCO-Transactions.git
+git push -u origin main
